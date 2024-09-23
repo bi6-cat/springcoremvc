c2f2fec0ddc7fc5686b27246cdc7c05a31351558
Zett <quangnn906@gmail.com>
2024-09-23 18:12:39 +0700

    Add Category entity

---
+++ src/main/java/com/example/entities/Category.java
@@ -0,0 +1,24 @@
+package com.example.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+    
+    @Column(name = "description")
+    private String description;
+
+}
