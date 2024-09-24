947bb03c14d5eb920a94e0384bcbe7b6ba765bc3
Zett <quangnn906@gmail.com>
2024-09-24 23:55:41 +0700

    Refactor CategoryController to include CRUD operations for categories

--- src/main/java/com/example/controllers/CategoryController.java
+++ src/main/java/com/example/controllers/CategoryController.java
@@ -1,5 +1,37 @@
 package com.example.controllers;
 
-public class CategoryController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.services.CategoryService;
+
+@Controller
+@RequestMapping("/category")
+public class CategoryController {
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public String index(Model model){
+        var categories = categoryService.findAll();
+        model.addAttribute("categories",categories);
+        return "category/index";
+    }
+
+    @GetMapping("/create")
+    public String create(){
+        return "category/create";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable int id){
+        categoryService.deleteById(id);
+        return "redirect:/category";
+    }
 }
